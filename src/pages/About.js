ccafe7442bfba9ac4171e716d3046b60741efc3c
LE TAN LOC <letanlocjp1001@gmail.com>
2021-12-06 03:34:39 +0900

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet Lorem
+        ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur Lorem ipsum
+        dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet,
+        consectetur Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem
+        ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet Lorem ipsum
+        dolor sit amet
+      </p>
+    </section>
+  )
+}
+
+export default About
